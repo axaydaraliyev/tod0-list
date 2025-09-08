cf6f77e3edbd64171719018ecca784cd30c9ec7c
a <a>
2025-09-08 16:42:14 +0500

    rename-projekt

--- js/todos.js
+++ js/todos.js
@@ -3,21 +3,6 @@ import { changerR } from "./changer.js";
 export let todos = localStorage.getItem("todos")
   ? JSON.parse(localStorage.getItem("todos"))
   : [];
-
-//  [
-//     {
-//         title: "todo 1",
-//         id : 1,
-
-//     },
-
-//        {
-//         title: "todo 2",
-//         id : 2,
-
-//     },
-// ];
-
 export function changeTodos(value) {
   todos = value;
   localStorage.setItem("todos", JSON.stringify(todos));
